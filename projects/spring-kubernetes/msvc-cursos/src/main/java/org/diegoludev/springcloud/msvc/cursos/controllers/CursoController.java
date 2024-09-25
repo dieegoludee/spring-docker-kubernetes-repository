0d1e4c939d40500b9a541adc2ae112d14b0f4ca5
Diego Ludeña <diegolude.martin@gmail.com>
2024-09-25 14:18:24 +0200

    Implementando métodos de PostMapping, PutMapping y DeleteMapping al Controlador de Cursos + Configuración de PostgreSQL en el application.properties

--- projects/spring-kubernetes/msvc-cursos/src/main/java/org/diegoludev/springcloud/msvc/cursos/controllers/CursoController.java
+++ projects/spring-kubernetes/msvc-cursos/src/main/java/org/diegoludev/springcloud/msvc/cursos/controllers/CursoController.java
@@ -3,10 +3,9 @@ package org.diegoludev.springcloud.msvc.cursos.controllers;
 import org.diegoludev.springcloud.msvc.cursos.entities.Curso;
 import org.diegoludev.springcloud.msvc.cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +26,32 @@ public class CursoController {
     Optional<Curso> optionalCurso = cursoService.buscarPorId(id);
     if (optionalCurso.isPresent()) {
       return ResponseEntity.ok(optionalCurso.orElseThrow());
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @PostMapping
+  public ResponseEntity<?> crearUsuario(@RequestBody Curso curso) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.guardarCurso(curso));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> editarCurso(@RequestBody Curso curso, @PathVariable Long id) {
+    Optional<Curso> optionalCurso = cursoService.buscarPorId(id);
+    if (optionalCurso.isPresent()) {
+      Curso cursoDb = optionalCurso.orElseThrow();
+      cursoDb.setNombre(curso.getNombre());
+      return ResponseEntity.status(HttpStatus.CREATED).body(cursoService.guardarCurso(cursoDb));
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> eliminarCurso(@PathVariable Long id) {
+    Optional<Curso> optionalCurso = cursoService.buscarPorId(id);
+    if (optionalCurso.isPresent()) {
+      cursoService.eliminarCurso(id);
+      return ResponseEntity.noContent().build();
     }
     return ResponseEntity.notFound().build();
   }
